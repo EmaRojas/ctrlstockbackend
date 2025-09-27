19897ba635db3e181f4f5c21469225d2d3edec9c
Ema Rojas <erojas@sancorseguros.com>
2025-09-27 18:50:11 -0300

    cambios

--- dist/index.js
+++ dist/index.js
@@ -9,18 +9,27 @@ const cors_1 = __importDefault(require("cors"));
 const dotenv_1 = __importDefault(require("dotenv"));
 const products_1 = __importDefault(require("./routes/products"));
 const cashRegister_1 = __importDefault(require("./routes/cashRegister"));
-dotenv_1.default.config();
 const app = (0, express_1.default)();
 const PORT = process.env;
-const allowedOrigins = [
-    "https://ctrolstockfront-git-caja-emarojas-projects.vercel.app",
-    "http://localhost:5173"
-];
 app.use((0, cors_1.default)({
-    origin: allowedOrigins,
+    origin: function (origin, callback) {
+        const allowedOrigins = [
+            "https://ctrolstockfront-git-caja-emarojas-projects.vercel.app",
+            "http://localhost:5173"
+        ];
+        // permitir solicitudes sin origin (por ejemplo Postman)
+        if (!origin)
+            return callback(null, true);
+        if (allowedOrigins.indexOf(origin) === -1) {
+            const msg = `El CORS para este origen no está permitido: ${origin}`;
+            return callback(new Error(msg), false);
+        }
+        return callback(null, true);
+    },
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     credentials: true
 }));
+dotenv_1.default.config();
 app.use(express_1.default.json());
 // Rutas
 app.use("/products", products_1.default);
